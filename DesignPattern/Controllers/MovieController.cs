2720dd410ee55d85241535a7f03a8df4b86d57aa
Mahmoud742 <89787004+Mahmoud742@users.noreply.github.com>
2022-12-16 22:01:38 +0200

    sec

--- DesignPattern/Controllers/MovieController.cs
+++ DesignPattern/Controllers/MovieController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DesignPattern.Controllers
 {
@@ -25,6 +26,53 @@ namespace DesignPattern.Controllers
         {
             var city=movieTicketSystemContext.Cities.Select(p=>p.Name).ToList();
             return Ok(city);
+        }   
+        [HttpGet("search by name")]
+        public async Task<IActionResult> Searchbyname(String name)
+        {
+
+            var moviesearch = movieTicketSystemContext.Movies.Where(s => s.Name!.Contains(name));
+
+
+            return Ok(moviesearch);
+
         }
+        [HttpGet("search by genere")]
+        public async Task<IActionResult> SearchbyGenere(String genere)
+        {
+
+            var moviesearch = movieTicketSystemContext.Movies.Where(s => s.Genre!.Contains(genere));
+
+
+            return Ok(moviesearch);
+        }
+        [HttpGet("search by Language")]
+        public async Task<IActionResult> Searchbylanguage(String language)
+        {
+
+            var moviesearch = movieTicketSystemContext.Movies.Where(s => s.Language!.Contains(language));
+
+
+            return Ok(moviesearch);
+        }
+        [HttpGet("search by releasedate")]
+        public async Task<IActionResult> Searchbydate(String date)
+        {
+
+            var moviesearch = movieTicketSystemContext.Movies.Where(s => s.Realeasedate!.Contains(date));
+
+
+            return Ok(moviesearch);
+        }
+        [HttpGet("get shows")]
+        public async Task<IActionResult> returnshows(String name)
+        {
+
+            var id = from Movie in movieTicketSystemContext.Movies select Movie.MoiveId;
+              var dee = id.Where(name.Equals(movieTicketSystemContext.Name)
+        }
+
+
+
     }
 }
